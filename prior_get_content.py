430ebc43623741f99303a9e48a5e9d7638d89ccc
Leonid Tepliakov <leotepl@gmail.com>
2024-02-24 21:59:23 +0300

    Initial version

---
+++ prior_get_content.py
@@ -0,0 +1,42 @@
+import urllib.request
+import psycopg2
+import argparse
+
+parser= argparse.ArgumentParser(description='Pull front page from prior.by')
+parser.add_argument('env',metavar='1',type=str,nargs='?',help='environment, prod or dev')
+args=parser.parse_args()
+
+def insert_content(content):
+    """ Insert into the table """
+
+    sql = """INSERT INTO raw.prior_web_content(content,insert_timestamp)
+             VALUES(%s,current_timestamp) RETURNING record_id;"""
+    
+    record_id = None
+#    config = load_config()
+
+    try:
+        with  psycopg2.connect(dbname="currency_rates_"+args.env, user="postgres",password="robirp", host="127.0.0.1", port="5432") as conn:
+            with  conn.cursor() as cur:
+                # execute the INSERT statement
+                cur.execute(sql, (content,))
+
+                # get the generated id back                
+                rows = cur.fetchone()
+                if rows:
+                    vendor_id = rows[0]
+
+                # commit the changes to the database
+                conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)    
+    finally:
+        return record_id
+    
+with urllib.request.urlopen('https://www.prior.by/web/') as f:
+    cont=f.read().decode('utf-8')
+
+# print(cont)
+
+insert_content(cont)    
+
